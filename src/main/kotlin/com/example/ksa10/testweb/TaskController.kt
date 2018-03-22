52cf7fa3b61d4892abbfa9e5503e5fd0e58e0a26
OFFICE <sign405@gmail.com>
2018-03-22 17:52:18 +0900

    Init Files

---
+++ src/main/kotlin/com/example/ksa10/testweb/TaskController.kt
@@ -0,0 +1,21 @@
+package com.example.ksa10.testweb
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("tasks")
+class TaskController {
+    @GetMapping
+    fun index(model: Model): String {
+        val tasks = listOf(
+                Task(1, "Test Task1", false),
+                Task(2, "Test Task2", true),
+                Task(3, "Test Task3", false)
+        )
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
+    }
+}
---
+++ src/main/kotlin/com/example/ksa10/testweb/TestwebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ksa10.testweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestwebApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestwebApplication>(*args)
+}
